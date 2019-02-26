63f1168429f712edf37227936e32a522ef2f5dbe
valentyn.dorosh <valentyn.dorosh@altium.com>
2019-02-26 17:02:25 +0200

    add validation errors on stream init

--- src/App.vue
+++ src/App.vue
@@ -90,19 +90,20 @@ export default {
         if (error.name === 'StreamApiNotSupportedError') {
           this.noStreamApiSupport = true
         }
-        // if (error.name === 'NotAllowedError') {
-        //   this.error = "ERROR: you need to grant camera access permisson"
-        // } else if (error.name === 'NotFoundError') {
-        //   this.error = "ERROR: no camera on this device"
-        // } else if (error.name === 'NotSupportedError') {
-        //   this.error = "ERROR: secure context required (HTTPS, localhost)"
-        // } else if (error.name === 'NotReadableError') {
-        //   this.error = "ERROR: is the camera already in use?"
-        // } else if (error.name === 'OverconstrainedError') {
-        //   this.error = "ERROR: installed cameras are not suitable"
-        // } else if (error.name === 'StreamApiNotSupportedError') {
-        //   this.error = "ERROR: Stream API is not supported in this browser"
-        // }
+
+        if (this.noStreamApiSupport) return
+
+        if (error.name === 'NotAllowedError') {
+          this.error = "ERROR: you need to grant camera access permisson"
+        } else if (error.name === 'NotFoundError') {
+          this.error = "ERROR: no camera on this device"
+        } else if (error.name === 'NotSupportedError') {
+          this.error = "ERROR: secure context required (HTTPS, localhost)"
+        } else if (error.name === 'NotReadableError') {
+          this.error = "ERROR: is the camera already in use?"
+        } else if (error.name === 'OverconstrainedError') {
+          this.error = "ERROR: installed cameras are not suitable"
+        }
       }
     }
   }
