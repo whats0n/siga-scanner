63c874011533c96c8137bdacb7314f5d55d99abd
valentyn.dorosh <valentyn.dorosh@altium.com>
2019-02-26 10:44:30 +0200

    add modal

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div id="app">
     <p class="decode-result">Last result: <b>{{ result }}</b></p>
-
-    <QrcodeDropZone
-      @init="logErrors"
-      @decode="onDecode"
-    >
-      <QrcodeStream
-        @init="onInit"
-        @decode="onDecode"
-      />
-    </QrcodeDropZone>
-
-    <QrcodeCapture
-      v-if="noStreamApiSupport"
-      @decode="onDecode"
-    />
+    <template v-if="noStreamApiSupport">
+      <QrcodeCapture @decode="onDecode"/>
+    </template>
+    <template v-else>
+      <div class="v-modal">
+        <QrcodeDropZone
+          class="v-modal__container"
+          @init="logErrors"
+          @decode="onDecode"
+        >
+          <QrcodeStream
+            @init="onInit"
+            @decode="onDecode"
+          />
+        </QrcodeDropZone>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+
+const fields = document.querySelectorAll('.js-qrcode-info')
 
 export default {
   name: 'app',
@@ -33,11 +37,20 @@ export default {
     result: '',
     noStreamApiSupport: false
   }),
+  watch: {
+    result (newValue) {
+      Array
+        .prototype
+        .forEach
+        .call(fields, field => {
+          field.value = newValue
+        })
+    }
+  },
   methods: {
     onDecode (result) {
       this.result = result
     },
-
     logErrors (promise) {
       promise.catch(console.error)
     },
@@ -53,3 +66,24 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+
+.v-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  &__container,
+  .qrcode-stream,
+  .qrcode-stream__inner-wrapper,
+  .qrcode-stream__pause-frame,
+  .qrcode-stream__camera {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+</style>
